352081b3d71f90d52a56da802a24bf2427ba5510
binini11 <biniam.y.yohannes>
2023-12-07 11:27:16 -0800

    python

--- 3-to_str.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Write a type-annotated function to_str that takes a float n
-as argument and returns the string representation of the float.
-"""
-
-def to_str(n: float) -> str:
-    """ REturns  the string reprenstation if the floart  """
-    return str(n)
